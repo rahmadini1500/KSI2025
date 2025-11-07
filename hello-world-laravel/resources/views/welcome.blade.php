7e46af411defc339223984766c64b62391e52af6
RahmadiniNurevaOctania <rahmadini2030@gmail.com>
2025-11-07 18:21:12 +0700

    Menghapus file HTML dan CSS lama

---
+++ hello-world-laravel/resources/views/welcome.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World - Laravel</title>
+    <style>
+        body {
+            background-color: #f9f9f9;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+            font-family: Arial, sans-serif;
+        }
+        h1 {
+            color: #2c3e50;
+            font-size: 60px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Hello World</h1>
+</body>
+</html>
